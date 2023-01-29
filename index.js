820abedcbe955203008d267379bba35175dbca79
dayanFreitas <dayan.freitas@catolicasc.org.br>
2023-01-29 18:26:07 -0300

    ## (Change Function Declaration)(153)

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function statement(invoice, plays) {
                   minimumFractionDigits: 2 }).format
   
   for(let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
