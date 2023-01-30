115b56aeff00f0995b133a546ae2c6bd9a012ed0
dayanFreitas <dayan.freitas@catolicasc.org.br>
2023-01-30 20:15:11 -0300

    (Change Function Declaration)(153)

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function statement(invoice, plays) {
   for(let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
     
-    result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result
 
@@ -58,10 +58,10 @@ function statement(invoice, plays) {
     return result
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
       { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format(aNumber)
+      minimumFractionDigits: 2 }).format(aNumber/100)
   }
 }
 
