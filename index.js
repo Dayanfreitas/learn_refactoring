1c92c2b2a15592c17d5311b1e1e76dd7129e549a
dayanFreitas <dayan.freitas@catolicasc.org.br>
2023-01-30 20:27:42 -0300

    ## Split Loop (257)

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function statement(invoice, plays) {
  
   for(let perf of invoice.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+
+  for(let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
