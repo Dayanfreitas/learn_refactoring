4d0fdd825f7d79f2bf2f609110979e625a04f855
dayanFreitas <dayan.freitas@catolicasc.org.br>
2023-01-30 21:00:06 -0300

    estrutura de dados

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ async function readFile (name) {
 }
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays)
+  const statementData = {}
+  return renderPlainText(statementData, invoice, plays)
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   
   for(let perf of invoice.performances) {
