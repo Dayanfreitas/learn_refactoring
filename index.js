69369cd74d4e5ae9e9794becfd015032fb8d0336
dayanFreitas <dayan.freitas@catolicasc.org.br>
2023-01-30 21:04:16 -0300

    estrutura de dados

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ async function readFile (name) {
 
 function statement(invoice, plays) {
   const statementData = {}
-  return renderPlainText(statementData, invoice, plays)
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays)
 }
 
-function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
   
-  for(let perf of invoice.performances) {
+  for(let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
@@ -66,7 +68,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for(let perf of invoice.performances) {
+    for(let perf of data.performances) {
       result += volumeCreditsFor(perf)
     }
     return result
@@ -74,7 +76,7 @@ function renderPlainText(data, invoice, plays) {
   
   function totalAmount() {
     let result = 0;
-    for(let perf of invoice.performances) {
+    for(let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
